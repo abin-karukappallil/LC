7396dea3697d29ce176b1949624befa7032a4a22
Abin Thomas <73917119+abin-karukappallil@users.noreply.github.com>
2024-04-08 23:04:15 +0530

    Create s.js

---
+++ sum/s.js
@@ -0,0 +1,22 @@
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+    let input = rl("nums=");
+    var nums=input.split(",");
+    var target=prompt("Target=");
+    for(i=0;i<nums.length;i++){
+        for(j=0;j<=i;j++){
+        var sum=nums[i] + nums[j];
+        if(sum==target){
+            var number=[nums.indexOf(i),nums.indexOf(j)];
+            console.log(number);
+            break;
+        }
+        else {
+            continue;
+        }
+
+        }
+    }
